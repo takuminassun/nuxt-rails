7d22b901858987b4bb1d835137c4d030775eef15
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-07-14 07:14:53 +0900

    user　一覧

---
+++ front/pages/users/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.name">
+        {{ user.name }}
+      </li>
+    </ul>
+    <router-link to="/users/new">新規登録</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ $axios }) {
+    return $axios.$get(`/api/users/`)
+      .then((res) => {
+        return { users: res }
+      })
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
